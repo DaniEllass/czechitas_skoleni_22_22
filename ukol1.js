03a01e19bc8ec3a5dbc4a26f18b35fdc69ba6b8f
DaniEllass <dan.souckova@gmail.com>
2022-01-22 11:44:06 +0100

    1 file modified, 1 added

---
+++ ukol1.js
@@ -0,0 +1,14 @@
+let vyska = 187;
+let vaha = 140;
+let vek = 87;
+let jmeno = "Barbucha";
+
+console.log(vyska);
+
+let bmi = vaha / (vyska * 2);
+
+console.log(bmi);
+
+console.log("Jmenuji se " + jmeno + "a je mi " + vek + ", vážím " + vaha + "kg a měřím " + vyska + "cm. Mé BMI je " + bmi + ".");
+
+
