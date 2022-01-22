a05a14d15eeea7aa36fa597c457c03dfa7dc762a
DaniEllass <dan.souckova@gmail.com>
2022-01-22 13:43:32 +0100

    1 file modified

--- ukol2.2.js
+++ ukol2.2.js
@@ -8,13 +8,13 @@ console.log ("Vaše zadaná čísla byla: " + a + ", " + b + ", " + c + ".");
 
 
 if (a > b && a > c) {
-console.log ("Největší hodnotou je číslo: " + a + ".");
+console.log ("Největší hodnotou je první číslo : " + a + ".");
 
 } else if (b > c && b > a) {
 
-    console.log ("Největší hodnotou je číslo: " + b + ".");
+    console.log ("Největší hodnotou je druhé číslo: " + b + ".");
 
 } else if (c > b && c > a) {
  
-    console.log ("Největší hodnotou je číslo: " + c + ".");
+    console.log ("Největší hodnotou je třetí číslo: " + c + ".");
 }
