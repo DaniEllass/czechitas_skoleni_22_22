64b07e58649b74588a0218732671b997d4bc31ae
DaniEllass <dan.souckova@gmail.com>
2022-01-22 13:06:18 +0100

    1 file modified

--- ukol2.2.js
+++ ukol2.2.js
@@ -3,4 +3,7 @@ let prvniCislo = (prompt ("Zadejte první číslo:"));
 let druheCislo = (prompt ("Zadejte druhé číslo:"));
 let tretiCislo = (prompt ("Zadejte třetí číslo:"));
 
+let soucet = (prvniCislo + druheCislo);
+
+console.log(soucet);
 
