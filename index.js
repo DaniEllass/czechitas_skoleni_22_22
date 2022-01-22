533e9875fe47512945b51bf0a9a8b147afd7cc35
DaniEllass <dan.souckova@gmail.com>
2022-01-22 11:56:12 +0100

    2 files modified

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import './ukol1.js';
 
+/*
 let otazka = prompt ("Kolik je ti let?");
 console.log(otazka);
 alert("Hej ty!");
+*/
+
+
--- ukol1.js
+++ ukol1.js
@@ -3,12 +3,16 @@ let vaha = 140;
 let vek = 87;
 let jmeno = "Barbucha";
 
-console.log(vyska);
 
 let bmi = vaha / (vyska * vyska);
 
-console.log(bmi);
-
-console.log("Jmenuji se " + jmeno + "a je mi " + vek + ", vážím " + vaha + "kg a měřím " + vyska + "cm. Mé BMI je " + bmi + ".");
+console.log(
+"Jmenuji se " + jmeno + 
+" a je mi " + vek + 
+", vážím " + vaha + 
+"kg a měřím " + vyska + 
+"cm. Mé BMI je "
+ + bmi + "."
+ );
 
 
