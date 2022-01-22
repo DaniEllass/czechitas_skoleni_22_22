b96ac9525842cc0543fe98ffdd3e2786d469572e
DaniEllass <dan.souckova@gmail.com>
2022-01-22 14:35:15 +0100

    1 file modified

--- ukol3.js
+++ ukol3.js
@@ -5,12 +5,13 @@
 
 3) Prepiste tento for cyklus pomoci While cyklu    for (let i = 12, j = 12 ** 3; i < 12 + j; i = i * 2) {      console.log(i);    }  - poznamka - pripomenout definici casti for cyklu */
 
-
 let zadaneCislo = Number(prompt ("Zadejte číslo:"))
-let soucet = 0
+let soucet1 = 0
 
 while (zadaneCislo !== 0) {
-  soucet = soucet + zadaneCislo;
+  soucet = soucet1 + zadaneCislo;
   cislo = Number(prompt("Zadej číslo:"));
   console.log("Akumulovaný součet = " + součet " - číslo: " + zadaneCislo)
 }
+
+console.log ("Zadali jste 0 hodnotu.")
